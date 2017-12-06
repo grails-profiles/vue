0b53a18fc94ea902c128b9eab6e6d0109a42033d
Zachary Klein <zak@silver-chalice.com>
2017-12-06 01:12:48 -0500

    Toggle between Vue links/Application status - added SERVER_URL config

--- skeleton/client/src/components/Welcome.vue
+++ skeleton/client/src/components/Welcome.vue
@@ -1,34 +1,61 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <a href="#" v-on:click="toggleLinks">{{showLinks ? 'View Application Status' : 'View Community Links'}}</a>
     <br/>
-    <h2>Application Status</h2>
-    <ul>
-      <li>Environment: <strong>{{ serverInfo ? serverInfo.environment : 'Loading' }}</strong></li>
-      <li>App Profile: <strong>{{ serverInfo ? serverInfo.appprofile : 'Loading' }}</strong></li>
+    <hr/>
+
+    <div v-if="!showLinks">
+      <h2>Application Status</h2>
+
+
+      <ul v-if="!showLinks">
+        <li>Environment: <strong>{{ serverInfo ? serverInfo.environment : 'Loading' }}</strong></li>
+        <li>App Profile: <strong>{{ serverInfo ? serverInfo.appprofile : 'Loading' }}</strong></li>
+        <br/>
+        <li>App version: <strong>{{ serverInfo ? serverInfo.appversion : 'Loading' }}</strong></li>
+        <li>Grails version: <strong>{{ serverInfo ? serverInfo.grailsversion : 'Loading' }}</strong></li>
+        <br/>
+        <li>Groovy version: <strong>{{ serverInfo ? serverInfo.groovyversion : 'Loading' }}</strong></li>
+        <li>JVM version: <strong>{{ serverInfo ? serverInfo.jvmversion : 'Loading' }}</strong></li>
+        <li>Reloading: <strong>{{ serverInfo ? serverInfo.reloadingagentenabled ? 'active' : 'inactive' : null}}</strong>
+        </li>
+      </ul>
       <br/>
-      <li>App version: <strong>{{ serverInfo ? serverInfo.appversion : 'Loading' }}</strong></li>
-      <li>Grails version: <strong>{{ serverInfo ? serverInfo.grailsversion : 'Loading' }}</strong></li>
+      <h2>Artefacts</h2>
+      <ul>
+        <li>Controllers: <strong>{{ serverInfo ? serverInfo.artefacts.controllers : 'Loading' }}</strong></li>
+        <li>Domains: <strong>{{ serverInfo ? serverInfo.artefacts.domains : 'Loading' }}</strong></li>
+        <li>Services: <strong>{{ serverInfo ? serverInfo.artefacts.services : 'Loading' }}</strong></li>
+      </ul>
       <br/>
-      <li>Groovy version: <strong>{{ serverInfo ? serverInfo.groovyversion : 'Loading' }}</strong></li>
-      <li>JVM version: <strong>{{ serverInfo ? serverInfo.jvmversion : 'Loading' }}</strong></li>
-      <li>Reloading: <strong>{{ serverInfo ? serverInfo.reloadingagentenabled ? 'active' : 'inactive' : null }}</strong>
-      </li>
-    </ul>
-    <br/>
-    <h2>Artefacts</h2>
-    <ul>
-      <li>Controllers: <strong>{{ serverInfo ? serverInfo.artefacts.controllers : 'Loading' }}</strong></li>
-      <li>Domains: <strong>{{ serverInfo ? serverInfo.artefacts.domains : 'Loading' }}</strong></li>
-      <li>Services: <strong>{{ serverInfo ? serverInfo.artefacts.services : 'Loading' }}</strong></li>
-    </ul>
-    <br/>
-    <h2>Installed Plugins</h2>
-    <ul v-if="serverInfo">
-      <li v-for="plugin in serverInfo.plugins">
-        {{ plugin.name }} - {{ plugin.version }}
-      </li>
-    </ul>
+      <h2>Installed Plugins</h2>
+      <ul v-if="serverInfo">
+        <li v-for="plugin in serverInfo.plugins">
+          {{ plugin.name }} - {{ plugin.version }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <h2>Essential Links</h2>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        <br>
+        <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -38,14 +65,19 @@
     data () {
       return {
         msg: 'Welcome to Your Grails & Vue.js App',
-        serverInfo: null
+        serverInfo: null,
+        showLinks: false,
+        serverURL: process.env.SERVER_URL
+      }
+    },
+    methods: {
+      toggleLinks: function () {
+        this.$data.showLinks = !this.$data.showLinks
       }
     },
     created: function () {
-      this.$http.get('http://localhost:8080/application').then(response => {
+      this.$http.get(`${this.$data.serverURL}/application`).then(response => {
         this.$data.serverInfo = response.body
-      }, response => {
-        console.warn(response)
       })
     }
   }
@@ -53,6 +85,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .hello {
+    width: 800px;
+    margin: 0 auto;
+  }
+
   h1, h2 {
     font-weight: normal;
   }
