fd248b5d347e211e5fc9883bf51409d0377099e8
Zachary Klein <zak@silver-chalice.com>
2017-12-11 21:08:13 -0600

    Implemented Grails landing page

--- skeleton/client/src/components/Welcome.vue
+++ skeleton/client/src/components/Welcome.vue
@@ -1,60 +1,99 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-
-    <a href="#" v-on:click="toggleLinks">{{showLinks ? 'View Application Status' : 'View Community Links'}}</a>
-    <br/>
-    <hr/>
-
-    <div v-if="!showLinks">
-      <h2>Application Status</h2>
+  <div>
 
 
-      <ul v-if="!showLinks">
-        <li>Environment: <strong>{{ serverInfo ? serverInfo.environment : 'Loading' }}</strong></li>
-        <li>App Profile: <strong>{{ serverInfo ? serverInfo.appprofile : 'Loading' }}</strong></li>
-        <br/>
-        <li>App version: <strong>{{ serverInfo ? serverInfo.appversion : 'Loading' }}</strong></li>
-        <li>Grails version: <strong>{{ serverInfo ? serverInfo.grailsversion : 'Loading' }}</strong></li>
-        <br/>
-        <li>Groovy version: <strong>{{ serverInfo ? serverInfo.groovyversion : 'Loading' }}</strong></li>
-        <li>JVM version: <strong>{{ serverInfo ? serverInfo.jvmversion : 'Loading' }}</strong></li>
-        <li>Reloading: <strong>{{ serverInfo ? serverInfo.reloadingagentenabled ? 'active' : 'inactive' : null}}</strong>
-        </li>
-      </ul>
-      <br/>
-      <h2>Artefacts</h2>
-      <ul>
-        <li>Controllers: <strong>{{ serverInfo ? serverInfo.artefacts.controllers : 'Loading' }}</strong></li>
-        <li>Domains: <strong>{{ serverInfo ? serverInfo.artefacts.domains : 'Loading' }}</strong></li>
-        <li>Services: <strong>{{ serverInfo ? serverInfo.artefacts.services : 'Loading' }}</strong></li>
-      </ul>
-      <br/>
-      <h2>Installed Plugins</h2>
-      <ul v-if="serverInfo">
-        <li v-for="plugin in serverInfo.plugins">
-          {{ plugin.name }} - {{ plugin.version }}
-        </li>
-      </ul>
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/#">
+            <img src="../assets/images/grails.svg" alt="Grails Logo"/>
+
+          </a>
+        </div>
+        <div class="navbar-collapse collapse" aria-expanded="false" style="height: 0.8px;">
+          <ul class="nav navbar-nav navbar-right">
+
+            <dropdown tag="li">
+              <btn type="primary" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                   aria-expanded="false">Application Status <span class="caret"></span></btn>
+              <template slot="dropdown">
+                <li v-if="serverInfo"><a href="#">Environment: {{serverInfo.environment}}</a></li>
+                <li v-if="serverInfo"><a href="#">App profile: {{serverInfo.appprofile}}</a></li>
+
+                <li role="separator" class="divider"></li>
+                <li v-if="serverInfo"><a href="#">Grails version: {{serverInfo.grailsversion}}</a></li>
+                <li v-if="serverInfo"><a href="#">Groovy version: {{serverInfo.groovyversion}}</a></li>
+                <li v-if="serverInfo"><a href="#">JVM version: {{serverInfo.jvmversion}}</a></li>
+                <li role="separator" class="divider"></li>
+                <li v-if="serverInfo"><a href="#">Reloading active: {{serverInfo.reloadingagentenabled ? 'true' :
+                  'false' }}</a></li>
+              </template>
+            </dropdown>
+            <dropdown tag="li">
+              <btn type="primary" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                   aria-expanded="false">Artefacts <span class="caret"></span></btn>
+              <template slot="dropdown" v-if="serverInfo">
+                <li v-if="serverInfo.artefacts"><a href="#">Controllers: {{serverInfo.artefacts.controllers}}</a></li>
+                <li v-if="serverInfo.artefacts"><a href="#">Domains: {{serverInfo.artefacts.domains}}</a></li>
+                <li v-if="serverInfo.artefacts"><a href="#">Services: {{serverInfo.artefacts.services}}</a></li>
+              </template>
+            </dropdown>
+            <dropdown tag="li">
+              <btn type="primary" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                   aria-expanded="false">Installed Plugins <span class="caret"></span></btn>
+              <template slot="dropdown" v-if="serverInfo">
+                <li v-for="plugin in serverInfo.plugins">
+                  <a href="#">{{ plugin.name }} - {{ plugin.version }}</a>
+                </li>
+
+              </template>
+            </dropdown>
+
+          </ul>
+        </div>
+      </div>
     </div>
-    <div v-else>
-      <h2>Essential Links</h2>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-        <br>
-        <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-      </ul>
-      <h2>Ecosystem</h2>
-      <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-      </ul>
+
+
+    <div class="svg" role="presentation">
+      <div class="grails-logo-container">
+        <img src="../assets/images/grails-cupsonly-logo-white.svg" class="grails-logo"/>
+        <span class="plus-logo">+</span>
+        <img src="../assets/logo.png" class="hero-log"/>
+      </div>
     </div>
+
+    <div id="content" role="main">
+      <section class="row colset-2-its">
+        <h1>Welcome to Grails</h1>
+
+        <p>
+          Congratulations, you have successfully started your Grails & Vue.js application!
+          While in development mode, changes will be loaded automatically when you edit your Vue.js app, without even
+          refreshing the page.
+          Below is a list of controllers that are currently deployed in
+          this application, click on each to execute its default action:
+        </p>
+
+        <div id="controllers" role="navigation">
+          <h2>Available Controllers:</h2>
+          <ul v-if="serverInfo">
+
+            <li v-if="serverInfo.controllers" v-for="controller in serverInfo.controllers" :key="controller.name">
+              <a v-bind:href="serverURL + '/' + controller.logicalPropertyName">{{controller.name }}</a></li>
+          </ul>
+        </div>
+      </section>
+    </div>
+
+
+    <div class="footer" role="contentinfo"></div>
 
   </div>
 </template>
@@ -72,39 +111,48 @@
     },
     methods: {
       toggleLinks: function () {
-        this.$data.showLinks = !this.$data.showLinks
+        this.showLinks = !this.$data.showLinks
       }
     },
     created: function () {
-      this.$http.get(`${this.$data.serverURL}/application`).then(response => {
-        this.$data.serverInfo = response.body
-      })
+      fetch(`${this.$data.serverURL}/application`)
+        .then(response => response.json())
+        .then(json => {
+          this.serverInfo = json
+        })
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .hello {
-    width: 800px;
-    margin: 0 auto;
+
+  .navbar-nav li button {
+    background: transparent;
+    border: none;
+    line-height: 20px;
+    font-size: 18px;
+    color: white;
+    padding: 25px 15px;
   }
 
-  h1, h2 {
-    font-weight: normal;
+  .btn-primary:active, .btn-primary.active, .open > .dropdown-toggle.btn-primary {
+    background-color: #e7e7e7;
   }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
+  button span.caret {
+    margin-left: 2px;
   }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
+  .plus-logo {
+    font-size: 10rem;
+    margin-left: -100px;
+    margin-right: -13px;
   }
 
-  a {
-    color: #42b983;
+  .hero-logo {
+    width: 161px;
+    margin-right: -161px;
+    margin-bottom: 88px;
   }
 </style>
--- skeleton/client/src/main.js
+++ skeleton/client/src/main.js
@@ -3,15 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import VueResource from 'vue-resource'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import * as uiv from 'uiv'
+import './assets/css/bootstrap.css'
+import './assets/css/grails.css'
+import './assets/css/main.css'
 
-Vue.use(BootstrapVue)
-Vue.use(VueResource)
 Vue.config.productionTip = false
 
+Vue.use(uiv)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
