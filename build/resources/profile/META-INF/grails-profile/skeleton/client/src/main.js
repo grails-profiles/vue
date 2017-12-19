ce6defcc12dbc91414fb27f7276f4a981247dc70
Zachary Klein <zak@silver-chalice.com>
2017-12-19 13:50:47 -0600

    Typo

---
+++ build/resources/profile/META-INF/grails-profile/skeleton/client/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as uiv from 'uiv'
+import './assets/css/bootstrap.css'
+import './assets/css/grails.css'
+import './assets/css/main.css'
+
+Vue.config.productionTip = false
+
+Vue.use(uiv)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
