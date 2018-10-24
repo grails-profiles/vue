4a8a6307a5b825cd969ba6e351dc34c6667f998e
Zachary Klein <zak@silver-chalice.com>
2018-10-23 19:03:33 -0500

    Added footer

--- skeleton/client/src/components/Welcome.vue
+++ skeleton/client/src/components/Welcome.vue
@@ -81,7 +81,36 @@
       </section>
     </div>
 
-    <div class="footer" role="contentinfo"></div>
+
+    <div class="footer row" role="contentinfo">
+      <div class="col-md-4">
+        <a href="http://guides.grails.org" target="_blank">
+          <img src="../assets/images/advancedgrails.svg" alt="Grails Guides" class="float-left"/>
+
+        </a>
+        <strong class="centered"><a href="http://guides.grails.org" target="_blank">Grails Guides</a></strong>
+        <p>Building your first Grails app? Looking to add security, or create a Single-Page-App? Check out the <a href="http://guides.grails.org" target="_blank">Grails Guides</a> for step-by-step tutorials.</p>
+
+      </div>
+      <div class="col-md-4">
+        <a href="http://docs.grails.org" target="_blank">
+          <img src="../assets/images/documentation.svg" alt="Grails Documentation" class="float-left"/>
+        </a>
+        <strong class="centered"><a href="http://docs.grails.org" target="_blank">Documentation</a></strong>
+        <p>Ready to dig in? You can find in-depth documentation for all the features of Grails in the <a href="http://docs.grails.org" target="_blank">User Guide</a>.</p>
+
+      </div>
+
+      <div class="col-md-4">
+        <a href="https://grails-slack.cfapps.io" target="_blank">
+          <img src="../assets/images/slack.svg" alt="Grails Slack" class="float-left"/>
+        </a>
+        <strong class="centered"><a href="https://grails-slack.cfapps.io" target="_blank">Join the Community</a></strong>
+        <p>Get feedback and share your experience with other Grails developers in the community <a href="https://grails-slack.cfapps.io" target="_blank">Slack channel</a>.</p>
+      </div>
+    </div>
+
+
 
   </div>
 </template>
